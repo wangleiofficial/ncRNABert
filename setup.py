be499096c001f376efcb3966b52731115c9b6da0
wnagleiofficial <798306069@qq.com>
2023-08-19 14:31:20 +0800

    add version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name='ncRNABert',
-      version='0.1.0',
+      version='0.1.1',
       description='ncRNA language model',
       long_description=long_description,
       long_description_content_type="text/markdown",
